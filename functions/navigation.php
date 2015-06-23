1bb7e2b9f44fc21a4b562d18ffe4d7f0f97ca638
Grey Hargreaves <shaun.hargreaves@gmail.com>
2015-06-23 08:26:34 +0100

    Move nav menu registration functions to nav function file

--- functions/navigation.php
+++ functions/navigation.php
@@ -1,6 +1,14 @@
 <?php
 
 /**
+ * Register navigation menus
+ */
+register_nav_menus(array('primary' => 'Main navigation'));
+register_nav_menus(array('secondary' => 'Secondary navigation'));
+register_nav_menus(array('footer' => 'Footer navigation'));
+register_nav_menus(array('social' => 'Social links'));
+
+/**
  * Custom navigation walker
  */
 class bb_navigation_walker extends Walker_Nav_Menu {
--- functions/theme.php
+++ functions/theme.php
@@ -1,14 +1,6 @@
 <?php
 
 /**
- * Register navigation menus
- */
-register_nav_menus(array('primary' => 'Main navigation'));
-register_nav_menus(array('secondary' => 'Secondary navigation'));
-register_nav_menus(array('footer' => 'Footer navigation'));
-register_nav_menus(array('social' => 'Social links'));
-
-/**
  * Allow article images and image resizing
  */
 add_theme_support('post-thumbnails');
