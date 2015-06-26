5f4a72f502d4e9f66180c63b5a5a5c78a28d7d6b
Grey Hargreaves <shaun.hargreaves@gmail.com>
2015-06-26 08:44:00 +0100

    Start work on global footer

--- functions/navigation.php
+++ functions/navigation.php
@@ -9,11 +9,10 @@ register_nav_menus(array('footer' => 'Footer navigation'));
 register_nav_menus(array('social' => 'Social links'));
 
 /**
- * Custom navigation walker
+ * Custom navigation walkers
  */
 class bb_navigation_walker extends Walker_Nav_Menu {
 	public function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
-		// print_r($item);
 		$attributes = '';
 		$classes = "navigation__item";
 		if(!empty($item->attr_title) && $item->attr_title !== $item->title) {
@@ -37,7 +36,6 @@ class bb_navigation_walker extends Walker_Nav_Menu {
 		$attributes = trim( $attributes );
 		$title = apply_filters("the_title", $item->title, $item->ID);
 		$item_output = "$args->before<a $attributes>$args->link_before$title$args->link_after <span class=\"icon icon--$icon navigation__icon\"></span></a>$args->after";
-		// Since $output is called by reference we don't need to return anything.
 		$output .= apply_filters(
 			'walker_nav_menu_start_el',
 			$item_output,
@@ -54,5 +52,34 @@ class bb_navigation_walker extends Walker_Nav_Menu {
 	}
 	public function end_lvl(&$output, $depth = 0, $args = array()) {
 		$output .= '</ul>';
+	}
+}
+
+class bb_naked_navigation_walker extends Walker_Nav_Menu {
+	public function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
+		$attributes = '';
+		if(!empty($item->url)) {
+			$attributes .= ' href="' . esc_attr($item->url) . '"';
+		}
+		$output .= "<li>";
+		$attributes = trim( $attributes );
+		$title = apply_filters("the_title", $item->title, $item->ID);
+		$item_output = "$args->before<a $attributes>$args->link_before$title</a>$args->link_after$args->after";
+		$output .= apply_filters(
+			'walker_nav_menu_start_el',
+			$item_output,
+			$item,
+			$depth,
+			$args
+		);
+	}
+	public function start_lvl(&$output, $depth = 0, $args = array()) {
+		$output .= '<ul>';
+	}
+	public function end_lvl(&$output, $depth = 0, $args = array()) {
+		$output .= '</ul>';
+	}
+	function end_el(&$output, $item, $depth = 0, $args = array()) {
+		$output .= '</li>';
 	}
 }
--- partials/global/footer.php
+++ partials/global/footer.php
@@ -1,4 +1,17 @@
 	<footer class="footer" id="footer" role="contentinfo">
-
+		<div class="footer__billboards">
+			<?php get_template_part('partials/global/billboards'); ?>
+		</div>
+		<div class="footer__boilerplate">
+			<?php
+				wp_nav_menu(array(
+					"theme_location" => "footer",
+					"menu_class" => "bb-boilerplate__links",
+					"container" => false,
+					"items_wrap" => '<ul class="%2$s">%3$s</ul>',
+					"walker" => new bb_naked_navigation_walker
+				));
+			?>
+		</div>
 	</footer>
 
