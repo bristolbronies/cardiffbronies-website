3b4312c77791756a4ff9421c4dd8672ad649084d
Grey Hargreaves <shaun.hargreaves@gmail.com>
2015-07-05 21:30:45 +0100

    Add function to kill orphans

--- src/js/scripts/app.ui.js
+++ src/js/scripts/app.ui.js
@@ -3,9 +3,18 @@ var app = app || {};
 app.ui = {
 	fuzzyDates: function() {
 		$("[data-timeago]").timeago();
+	},
+	killAllOrphans: function() {
+		$("h1, h2, h3, h4, h5, h6").each(function(index, element) {
+			$(this).html($(this).text().replace(/\s(?=[^\s]*$)/g, "&nbsp;"));
+		});
 	}
 }
 
 $(document).ready(function() {
 	app.ui.fuzzyDates();
+});
+
+$(window).load(function() {
+	app.ui.killAllOrphans();
 });
