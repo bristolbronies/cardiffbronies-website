d1e222f82d6791c1e2e9c3647e396d31dcb2f6f3
Grey Hargreaves <shaun.hargreaves@gmail.com>
2015-07-06 10:41:47 +0100

    Remove old code from billboards partial

--- partials/global/billboards.php
+++ partials/global/billboards.php
@@ -20,28 +20,3 @@
 	endif;
 	wp_reset_postdata();
 ?>
-
-<?php
-	/* $billboards = new WP_Query('post_type=billboard&orderby=date&order=DESC&posts_per_page=2');
-	if($billboards->have_posts()):
-		while($billboards->have_posts()): $billboards->the_post(); 
-?>
-					<figure class="billboard billboard--inline">
-						<?php
-							$image_url = get_field("billboard_image"); 
-						?>
-						<div class="billboard__media">
-							<a href="<?php echo get_field("billboard_url"); ?>">
-								<img src="<?php echo $image_url['url']; ?>" alt="<?php the_title(); ?>">
-							</a>
-						</div>
-						<figcaption class="billboard__caption">
-							<h4 class="billboard__title"><a href="<?php echo get_field("billboard_url"); ?>"><?php the_title(); ?></a></h4>
-							<?php the_excerpt(); ?>
-						</figcaption>
-					</figure>
-<?php
-		endwhile;
-	endif; 
-	wp_reset_postdata();*/
-?>
