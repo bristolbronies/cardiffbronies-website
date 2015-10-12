bf873eedbe4f8235080c49bbf6746bd7ed674f1c
Grey Hargreaves <shaun.hargreaves@gmail.com>
2015-10-12 20:48:42 +0100

    Correct site name in footer

--- partials/global/footer.php
+++ partials/global/footer.php
@@ -3,7 +3,7 @@
 			<?php get_template_part('partials/global/billboards'); ?>
 		</div>*/ ?>
 		<div class="footer__boilerplate">
-			<small class="footer__legal">&copy;<?php echo date("Y"); ?> <?php echo get_bloginfo("name"); ?>. All rights reserved. Website designed and developed by <a href="http://greysadventures.com/">Grey's Adventures</a>. <em>My Little Pony: Friendship is Magic</em> is &copy; Hasbro. Bristol Bronies is not affiliated in any way with Hasbro or DHX Media. No copyright infringement intended.</small>
+			<small class="footer__legal">&copy;<?php echo date("Y"); ?> <?php echo get_bloginfo("name"); ?>. All rights reserved. Website designed and developed by <a href="http://greysadventures.com/">Grey's Adventures</a>. <em>My Little Pony: Friendship is Magic</em> is &copy; Hasbro. <?php bloginfo("name"); ?> is not affiliated in any way with Hasbro or DHX Media. No copyright infringement intended.</small>
 			<?php
 				wp_nav_menu(array(
 					"theme_location" => "footer",
