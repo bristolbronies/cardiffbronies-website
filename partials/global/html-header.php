d74cee93eb666b12f250df05077f00df6f98ffe2
Grey Hargreaves <shaun.hargreaves@gmail.com>
2015-10-12 11:52:50 +0100

    Add all the favicons

--- partials/global/html-header.php
+++ partials/global/html-header.php
@@ -19,6 +19,29 @@
 	<meta name="ICBM" content="51.454513, -2.58791">
 
 	<!-- Icons -->
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-57x57.png?v=RyywKjJ0xk">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-60x60.png?v=RyywKjJ0xk">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-72x72.png?v=RyywKjJ0xk">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-76x76.png?v=RyywKjJ0xk">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-114x114.png?v=RyywKjJ0xk">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-120x120.png?v=RyywKjJ0xk">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-144x144.png?v=RyywKjJ0xk">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-152x152.png?v=RyywKjJ0xk">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-180x180.png?v=RyywKjJ0xk">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/favicon-32x32.png?v=RyywKjJ0xk" sizes="32x32">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/favicon-194x194.png?v=RyywKjJ0xk" sizes="194x194">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/favicon-96x96.png?v=RyywKjJ0xk" sizes="96x96">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/android-chrome-192x192.png?v=RyywKjJ0xk" sizes="192x192">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/favicon-16x16.png?v=RyywKjJ0xk" sizes="16x16">
+	<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/manifest.json?v=RyywKjJ0xk">
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/favicon.ico?v=RyywKjJ0xk">
+	<meta name="apple-mobile-web-app-title" content="Bristol Bronies">
+	<meta name="application-name" content="Bristol Bronies">
+	<meta name="msapplication-TileColor" content="#e02437">
+	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/mstile-144x144.png?v=RyywKjJ0xk">
+	<meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/browserconfig.xml?v=RyywKjJ0xk">
+	<meta name="theme-color" content="#e02437">
+	<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/safari-icon.svg?v=RyywKjJ0xk" color="#e02437">
 
 	<!-- Facebook OpenGraph -->
 	<meta property="og:title" content="">
